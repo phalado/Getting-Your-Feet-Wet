c855f32d5c12b0a667f6a1228c6db6d170ec2d9d
Phalado <phalado@gmail.com>
2019-11-29 13:41:21 -0300

    still fighting the heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, heroku!"
+    render html: "hello, heroku! again..."
   end
 end
