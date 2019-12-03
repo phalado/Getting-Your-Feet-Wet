308aaca7eebb5b4d97e75e080cd93d26ad860c3e
Phalado <phalado@gmail.com>
2019-12-03 12:32:15 -0300

    dit rubocop corrections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, heroku! again..."
+    render html: 'hello, heroku! again...'
   end
 end
